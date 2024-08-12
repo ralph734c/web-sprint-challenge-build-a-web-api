1021edc395d6311a9259b00cccf9a1a943dc3c61
Christopher Ralph <chris.734c@gmail.com>
2024-08-12 01:08:39 -0400

    👍 IMPROVE: Completed the initial pass at the projects router and various endpoints.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -7,16 +7,30 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     console.log(`${req.method} to ${req.originalUrl}`);
+    const projects = await projectsModel.get();
+    if (projects) {
+      res.status(200).json(projects);
+    } else {
+      res.status(200).json([]);
+    }
   } catch (error) {
     next(error);
   }
 });
 
 router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
-    console.log(
-      `${req.method} to ${req.originalUrl} with ID: ${req.params.id}`
-    );
+    console.log(`${req.method} to ${req.originalUrl} with ID: ${id}`);
+    const project = await projectsModel.get(id);
+
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({
+        message: `A project with ID: ${id} was not found, try again!`,
+      });
+    }
   } catch (error) {
     next(error);
   }
@@ -25,6 +39,15 @@ router.get('/:id', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     console.log(`${req.method} to ${req.originalUrl}`);
+    const { name, description } = req.body;
+    if (name && description && name.trim() && description.trim()) {
+      const newProject = await projectsModel.insert(req.body);
+      res.status(201).json(newProject);
+    } else {
+      res.status(400).json({
+        message: 'Creating a new project requires the name and description',
+      });
+    }
   } catch (error) {
     next(error);
   }
@@ -35,33 +58,67 @@ router.put('/:id', async (req, res, next) => {
     console.log(
       `${req.method} to ${req.originalUrl} with ID: ${req.params.id}`
     );
+    const { name, description } = req.body;
+    const id = req.params.id;
+    if (name && description && name.trim() && description.trim()) {
+      const updatedProject = await projectsModel.update(id, req.body);
+      if (updatedProject) {
+        res.status(201).json(updatedProject);
+      } else {
+        res.status(404).json({
+          message: `A project with ID: ${id} was not found, try again!`,
+        });
+      }
+    } else {
+      res.status(400).json({
+        message: 'Creating a new project requires the name and description',
+      });
+    }
   } catch (error) {
     next(error);
   }
 });
 
 router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
     console.log(
       `${req.method} to ${req.originalUrl} with ID: ${req.params.id}`
     );
+    const projectToDelete = await projectsModel.remove(id);
+
+    if (projectToDelete) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).json({
+        message: `A project with ID: ${id} was not found, try again!`,
+      });
+    }
   } catch (error) {
     next(error);
   }
 });
 
 router.get('/:id/actions', async (req, res, next) => {
+  const { id } = req.params;
   try {
     console.log(
       `${req.method} to ${req.originalUrl} with ID: ${req.params.id}`
     );
+    const projActions = await projectsModel.getProjectActions(id);
+    if (projActions) {
+      res.status(200).json(projActions);
+    } else {
+      res
+        .status(404)
+        .json(`A project with ID: ${id} was not found, try again!`);
+    }
   } catch (error) {
     next(error);
   }
 });
 
-router.use((error, req, res, next) => {
-  // eslint-disable-line
+router.use((error, req, res, next) => { // eslint-disable-line
   res.status(error.status || 500).json({
     customMessage: 'Something bad happened in the projects router',
     message: error.message,
