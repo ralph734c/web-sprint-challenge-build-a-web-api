9685740d4d4507eb33fdea5dcb48e0a3d69787b6
Christopher Ralph <chris.734c@gmail.com>
2024-08-11 23:49:18 -0400

    📦 NEW: Initial server setup and scripts

--- api/server.js
+++ api/server.js
@@ -6,4 +6,10 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's wire up these APIs, routes, and middleware!</h2>`)
+})
+
 module.exports = server;
